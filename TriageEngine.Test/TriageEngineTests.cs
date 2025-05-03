fd73195a72482ee75108601259d4d2cd4bf1af01
Christoffer Ljungqvist <Christoffer.Ljungqvist@hotmail.se>
2025-05-03 15:05:08 +0200

    Created TriageEngine

---
+++ TriageEngine.Test/TriageEngineTests.cs
@@ -0,0 +1,22 @@
+﻿using Shouldly;
+using TriageEngine.Models;
+
+namespace TriageEngine.Test;
+
+public class TriageEngineTests
+{
+    [Fact]
+    public void ProcessTriage_ReturnsTriage()
+    {
+        var triageEngine = CreateSut();
+
+        var result = triageEngine.ProcessTriage("Form");
+
+        result.ShouldBeAssignableTo<Triage>();
+        result.FormId.ShouldBe("TestForm");
+        result.Questions.Count().ShouldBe(3);
+    }
+
+    private static TriageEngine CreateSut() =>
+        new TriageEngine();
+}
---
+++ TriageEngine/ITriageService.cs
@@ -0,0 +1,8 @@
+﻿using TriageEngine.Models;
+
+namespace TriageEngine;
+
+public interface ITriageService
+{
+    Triage ProcessTriage(string formId);
+}
